24f20d023918c09f8a7b2460ac2c10c19aac4a30
JohnMakous <39932037+JohnMakous@users.noreply.github.com>
2024-06-23 14:21:55 -0400

    Update main.py copy 1littlecoder code

--- main.py
+++ main.py
@@ -3,6 +3,49 @@ import numpy as np
 from pyweb import pydom
 from pyscript import display
 
+import asyncio
+import js
+from js import document, FileReader
+from pyodide import create_proxy
+   
+  def read_complete(event):
+    # event is ProgressEvent
+   
+    content = document.getElementById("content");
+    content.innerText = event.target.result
+
+   
+  async def process_file(x):
+    fileList = document.getElementById('upload').files
+   
+    for f in fileList:
+      # reader is a pyodide.JsProxy
+      reader = FileReader.new()
+   
+      # Create a Python proxy for the callback function
+      onload_event = create_proxy(read_complete)
+
+      #console.log("done")
+   
+      reader.onload = onload_event
+   
+      reader.readAsText(f)
+   
+    return
+   
+  def main():
+    # Create a Python proxy for the callback function
+    file_event = create_proxy(process_file)
+   
+    # Set the listener to the callback
+    e = document.getElementById("upload")
+    e.addEventListener("change", file_event, False)
+   
+  main()
+   
+
+
+
 def plot_spectrum(event):
     data_filename = pydom["input#filename"][0].value
     data_set = np.loadtxt(data_filename+".csv", delimiter=',')
