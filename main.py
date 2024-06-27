bd4a60e345ba05513f21922e52a4ed5c308d2f7c
JohnMakous <39932037+JohnMakous@users.noreply.github.com>
2024-06-27 07:52:46 -0400

    Update main.py move pyscript code here

--- main.py
+++ main.py
@@ -2,13 +2,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pyweb import pydom
 from pyscript import display
-
+import panel as pn
+import pandas as pd
+from panel.io.pyodide import show
 import asyncio
-import js
-from js import document, window, Uint8Array
 from pyodide.ffi import create_proxy
 from pyodide.ffi.wrappers import add_event_listener
-   
+
+
+fileInput = pn.widgets.FileInput(accept='.csv')
+uploadButton = pn.widgets.Button(name='Upload', button_type='primary')
+		
+table = pn.widgets.Tabulator(pagination='remote', page_size=10)
+
+document.getElementById('table').style.display = 'none'
+
+async def process_file(event):
+	if fileInput.value is not None:
+		table.value = pd.read_csv(io.BytesIO(fileInput.value))
+		document.getElementById('table').style.display = 'block'
+		
+uploadButton.on_click(process_file)
+
+await show(fileInput, 'fileinput')
+await show(uploadButton, 'upload')	
+await show(table, 'table')
+
+
 async def upload_file_and_show(e):
    file_list = e.target.files
    first_item = file_list.item(0)
